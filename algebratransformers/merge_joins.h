eaa4d0d1d325f9dbb09717ba5bb9ab428c508919
Janusz Lewandowski <lew21@xtreeme.org>
2015-11-21 03:34:30 +0100

    Add a merge_joins transformer.

---
+++ algebratransformers/merge_joins.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "../algebratransformer.h"
+
+namespace sparqlxx
+{
+	namespace AlgebraTransformers
+	{
+		struct MergeJoins : public AlgebraTransformer
+		{
+			using AlgebraTransformer::transform;
+
+			auto transform(Algebra::Join&& op) -> Algebra::Op override
+			{
+				auto new_ops = std::vector<Algebra::AnyOp>{};
+				std::function<void(Algebra::Join&&)> handle_join = [&new_ops, &handle_join](Algebra::Join&& op) -> void{
+					for (auto& subop : op.ops)
+					{
+						if (subop->is<Algebra::Join>())
+							handle_join(std::move(subop->get<Algebra::Join>()));
+						else
+							new_ops.emplace_back(subop);
+					}
+				};
+				handle_join(std::move(op));
+				op.ops = new_ops;
+				return op;
+			}
+		};
+
+		auto merge_joins(Algebra::Op&& op) -> Algebra::Op
+		{
+			return MergeJoins()(std::move(op));
+		}
+	}
+}
--- tests/test_transformers.cpp
+++ tests/test_transformers.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
 
 #include "../algebratransformers/to_quads.h"
+#include "../algebratransformers/merge_joins.h"
 #include "../to_sse.h"
-
+#include <boost/algorithm/string/replace.hpp>
 
 using namespace sparqlxx;
+
+template <typename T>
+auto to_unformatted_sse(const T& val)
+{
+	auto s = to_sse(val);
+	boost::algorithm::replace_all(s, "\t", "");
+	boost::algorithm::replace_all(s, "\n", " ");
+	s = s.substr(0, s.size()-1);
+	return s;
+}
 
 void test_to_quads()
 {
@@ -12,28 +23,56 @@ void test_to_quads()
 
 	{
 		op = Algebra::Basic{{TripleVP{Iri{"a"}, Iri{"b"}, Iri{"c"}}}};
-		assert(to_sse(op) == "(bgp [<a> <b> <c>])\n");
+		assert(to_unformatted_sse(op) == "(bgp [<a> <b> <c>])");
 		op = AlgebraTransformers::to_quads(std::move(op), Iri{"G"});
-		assert(to_sse(op) == "(quadpattern [<G> <a> <b> <c>])\n");
+		assert(to_unformatted_sse(op) == "(quadpattern [<G> <a> <b> <c>])");
 	}
 
 	{
 		op = Algebra::Graph{Iri{"X"}, Algebra::make<Algebra::Basic>(TriplesVP{TripleVP{Iri{"a"}, Iri{"b"}, Iri{"c"}}})};
-		assert(to_sse(op) == "(graph <X>\n\t(bgp [<a> <b> <c>])\n)\n");
+		assert(to_unformatted_sse(op) == "(graph <X> (bgp [<a> <b> <c>]) )");
 		op = AlgebraTransformers::to_quads(std::move(op), Iri{"G"});
-		assert(to_sse(op) == "(quadpattern [<X> <a> <b> <c>])\n");
+		assert(to_unformatted_sse(op) == "(quadpattern [<X> <a> <b> <c>])");
 	}
 
 	{
 		op = Algebra::Join{Algebra::make<Algebra::Graph>(Iri{"X"}, Algebra::make<Algebra::Basic>(TriplesVP{TripleVP{Iri{"a"}, Iri{"b"}, Iri{"c"}}})), Algebra::make<Algebra::Basic>(TriplesVP{TripleVP{Iri{"a"}, Iri{"b"}, Iri{"c"}}})};
-		assert(to_sse(op) == "(join\n\t(graph <X>\n\t\t(bgp [<a> <b> <c>])\n\t)\n\t(bgp [<a> <b> <c>])\n)\n");
+		assert(to_unformatted_sse(op) == "(join (graph <X> (bgp [<a> <b> <c>]) ) (bgp [<a> <b> <c>]) )");
 		op = AlgebraTransformers::to_quads(std::move(op), Iri{"G"});
-		assert(to_sse(op) == "(join\n\t(quadpattern [<X> <a> <b> <c>])\n\t(quadpattern [<G> <a> <b> <c>])\n)\n");
+		assert(to_unformatted_sse(op) == "(join (quadpattern [<X> <a> <b> <c>]) (quadpattern [<G> <a> <b> <c>]) )");
+	}
+}
+
+void test_merge_joins()
+{
+	auto op = Algebra::Op{Algebra::Null{}};
+
+	{
+		op = Algebra::Join{
+			Algebra::make<Algebra::Graph>(Iri{"X"},
+				Algebra::make<Algebra::Basic>(TriplesVP{TripleVP{Iri{"a"}, Iri{"b"}, Iri{"c"}}})
+			),
+			Algebra::make<Algebra::Join>(
+				Algebra::make<Algebra::Join>(
+					Algebra::make<Algebra::Basic>(TriplesVP{TripleVP{Iri{"a"}, Iri{"b"}, Iri{"d"}}}),
+					Algebra::make<Algebra::Basic>(TriplesVP{TripleVP{Iri{"a"}, Iri{"b"}, Iri{"e"}}})
+				),
+				Algebra::make<Algebra::Join>(
+					Algebra::make<Algebra::Basic>(TriplesVP{TripleVP{Iri{"a"}, Iri{"b"}, Iri{"f"}}}),
+					Algebra::make<Algebra::Basic>(TriplesVP{TripleVP{Iri{"a"}, Iri{"b"}, Iri{"g"}}})
+				)
+			)
+		};
+		assert(to_unformatted_sse(op) == "(join (graph <X> (bgp [<a> <b> <c>]) ) (join (join (bgp [<a> <b> <d>]) (bgp [<a> <b> <e>]) ) (join (bgp [<a> <b> <f>]) (bgp [<a> <b> <g>]) ) ) )");
+		op = AlgebraTransformers::merge_joins(std::move(op));
+		assert(to_unformatted_sse(op) == "(join (graph <X> (bgp [<a> <b> <c>]) ) (bgp [<a> <b> <d>]) (bgp [<a> <b> <e>]) (bgp [<a> <b> <f>]) (bgp [<a> <b> <g>]) )");
+//		std::cout << to_unformatted_sse(op) << std::endl;
 	}
 }
 
 int main()
 {
 	test_to_quads();
+	test_merge_joins();
 	return 0;
 }
